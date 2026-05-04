0dc3ff62797b521742e63ba7789a9f118a276a37
darzokako-maker <darzokako@gmail.com>
2026-05-04 18:02:03 +0300

    Add Android.mk for BlockForceMenu module

---
+++ jni/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := BlockForceMenu
+
+# ../ ile üst klasördeki imgui dosyalarına erişiyoruz
+LOCAL_SRC_FILES := main.cpp \
+                   ../imgui.cpp \
+                   ../imgui_draw.cpp \
+                   ../imgui_widgets.cpp \
+                   ../imgui_tables.cpp \
+                   ../imgui_impl_android.cpp \
+                   ../imgui_impl_opengl3.cpp
+
+LOCAL_LDLIBS := -llog -lGLESv2 -landroid
+LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/..
+
+include $(BUILD_SHARED_LIBRARY)
+
