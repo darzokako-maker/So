7619ee0547362137ea5ebf0c435ee02f0b4d6cc7
darzokako-maker <darzokako@gmail.com>
2026-05-04 18:09:00 +0300

    Refactor source file paths in Android.mk
    Updated source file paths in Android.mk to reflect new structure.

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,17 +3,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := BlockForceMenu
 
-# Eğer imgui.cpp ana dizindeyse ../ kullanılır
+# Tüm dosyalar artık aynı jni klasöründe olduğu için doğrudan isimlerini yazıyoruz
 LOCAL_SRC_FILES := main.cpp \
-                   ../imgui.cpp \
-                   ../imgui_draw.cpp \
-                   ../imgui_widgets.cpp \
-                   ../imgui_tables.cpp \
-                   ../imgui_demo.cpp \
-                   ../imgui_impl_android.cpp \
-                   ../imgui_impl_opengl3.cpp
+                   imgui.cpp \
+                   imgui_draw.cpp \
+                   imgui_widgets.cpp \
+                   imgui_tables.cpp \
+                   imgui_impl_android.cpp \
+                   imgui_impl_opengl3.cpp
 
 LOCAL_LDLIBS := -llog -lGLESv2 -landroid
-LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/..
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
 include $(BUILD_SHARED_LIBRARY)
